b5d3067a27ee5e83ac24c6fc1d795945bbc5edaf
Lara Bernardes <contatolarabernardes@gmail.com>
2024-04-22 12:10:34 +0300

    Added controller for Product

--- src/main/java/lv/venta/controler/FirstController.java
+++ src/main/java/lv/venta/controler/FirstController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lv.venta.model.Product;
+
 
 
 @Controller
@@ -26,6 +28,15 @@ public class FirstController {
 		return "hello-msg-page";
 	}
 	
+	
+	@GetMapping("/product/test") //localhost:8080/product/test
+	public String getProductTest(Model model) {
+		System.out.println("The thid controller is working!!");
+		Product p = new Product("Apple", "red", 0.99f, 4);
+		model.addAttribute("mypackage", p );
+		return "product-test";
+		
+	}
 	
 	
 }
